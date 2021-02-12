d6cd12503f2c14fe2c0ad450b0b9c6b11ad2b37e
dms-mrk <mariko-mamadou@outlook.fr>
2021-02-12 15:00:40 +0100

    Update ProductTest.php

--- tests/Products/ProductTest.php
+++ tests/Products/ProductTest.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Tests\Products;
-
 
 use App\Entity\Products;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -15,20 +13,20 @@ class ProductTest extends WebTestCase
         $time = new \DateTime('now', $timezone);
 
         $product = new Products();
-        $product->setName("Iphone12");
-        $product->setDescription("Le dernier telephone tendance");
-        $product->setCover("testimage.jpg");
-        $product->setPrice("10.99");
+        $product->setName('Iphone12');
+        $product->setDescription('Le dernier telephone tendance');
+        $product->setCover('testimage.jpg');
+        $product->setPrice('10.99');
         $product->setQuantity(12);
         $product->setDateC($time);
         $product->setDateM($time);
 
-        $this->assertSame("Iphone12",$product->getName());
-        $this->assertSame("Le dernier telephone tendance",$product->getDescription());
-        $this->assertSame("testimage.jpg",$product->getCover());
-        $this->assertSame("10.99",$product->getPrice());
-        $this->assertSame(12,$product->getQuantity());
-        $this->assertSame($time,$product->getDateC());
-        $this->assertSame($time,$product->getDateM());
+        $this->assertSame('Iphone12', $product->getName());
+        $this->assertSame('Le dernier telephone tendance', $product->getDescription());
+        $this->assertSame('testimage.jpg', $product->getCover());
+        $this->assertSame('10.99', $product->getPrice());
+        $this->assertSame(12, $product->getQuantity());
+        $this->assertSame($time, $product->getDateC());
+        $this->assertSame($time, $product->getDateM());
     }
 }
