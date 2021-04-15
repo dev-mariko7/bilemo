c0acb28149d62cd0e3d82bbbfc440403c20fad5d
dms-mrk <mariko-mamadou@outlook.fr>
2021-04-15 17:15:25 +0200

    Update catchException.php

--- src/Controller/Exceptions/catchException.php
+++ src/Controller/Exceptions/catchException.php
@@ -4,13 +4,13 @@
 namespace App\Controller\Exceptions;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-
+/*
 class catchException implements EventSubscriberInterface
 {
     public static function getSubscribedEvents()
     {
         /*return [
             '' => ''
-        ]*/
+        ]
     }
-}
+}*/
