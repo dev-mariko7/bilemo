1f2b1e5684ae7969cd4cc021bc4fc3174a22d75e
dms-mrk <mariko-mamadou@outlook.fr>
2021-04-15 17:10:23 +0200

    Create catchException.php

---
+++ src/Controller/Exceptions/catchException.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller\Exceptions;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class catchException implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        /*return [
+            '' => ''
+        ]*/
+    }
+}
